9bf26fac5986b53bd67104f3b1e3c2a5df44e3d6
Memo <77158623+Selle007@users.noreply.github.com>
2023-02-17 01:20:36 +0100

    Initial Project Setup

---
+++ MEVN E-commerce/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <body>
+    <nav>
+      <NavBar />
+    </nav>
+    <main>
+<h1>HOME</h1>
+    </main>
+
+
+  </body>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue';
+
+
+</script>
+
+<style scoped>
+body{
+  width: 100%;
+}
+</style>
---
+++ MEVN E-commerce/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="navbar">
+        <nav>
+        <img src="https://th.bing.com/th/id/R.0b333471aa2e67d8acf503abb80a2420?rik=y5ilHVYvvz3lcg&pid=ImgRaw&r=0"/>
+        <div class="logo">
+        </div>
+        
+        <div class="links">
+        <router-link to="/"> Home </router-link>
+            <router-link to="/feed"> Feed </router-link>
+            </div>
+            <div class="search">
+            
+            </div>
+
+
+
+            </div>
+           
+        </nav>
+
+        <router-view />
+    </div>
+</template>
+
+
+<style scoped>
+.navbar {
+    overflow: hidden;
+    background-color: #fff;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+
+a {
+    color: #000;
+}
+</style>
